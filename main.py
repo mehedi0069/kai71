c4244fca134ad0c9a207996da63526b4c9773e3e
mehedi0069 <160133281+mehedi0069@users.noreply.github.com>
2025-09-19 17:31:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ url = "https://siyamahmmed.shop/jimkarr.php"
 
 while True:
     try:
-        response = requests.get(url, timeout=3)
+        response = requests.get(url, timeout=10)
         print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] [jimkar bot] {url} → {response.status_code}")
     except Exception as e:
         print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] [jimkar bot] Error: {e}")
