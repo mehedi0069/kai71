aff7e6d8c5edd4fdb65b8b3454705a12737ab430
mehedi0069 <160133281+mehedi0069@users.noreply.github.com>
2025-09-19 17:38:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ while True:
         print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] [jimkar bot] {url} → {response.status_code}")
     except Exception as e:
         print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] [jimkar bot] Error: {e}")
-    time.sleep(2)
+    time.sleep(3)
