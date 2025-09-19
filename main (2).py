e1e644ad058b6899cb9f36df643d39784a8e170b
mehedi0069 <160133281+mehedi0069@users.noreply.github.com>
2025-09-19 17:00:59 +0600

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+import time
+import requests
+
+url = "https://siyamahmmed.shop/jimkarr.php"
+
+while True:
+    try:
+        response = requests.get(url, timeout=10)
+        print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] [jimkar bot] {url} → {response.status_code}")
+    except Exception as e:
+        print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] [jimkar bot] Error: {e}")
+    time.sleep(2)
